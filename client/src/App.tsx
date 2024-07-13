6d861cad44f90cc92a3156b4b525a7f8f9e267b7
phillipdunn <phillipdunn@hotmail.com>
2024-07-13 16:51:32 +0100

    chore: add fetch to get api data and check contract between front and back

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,19 @@
 import { Box } from '@mui/material';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 
 function App() {
+  const [data, setData] = useState([{}]);
+  useEffect(() => {
+    fetch('/api')
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setData(data)
+      });
+  }, []);
+
+  console.log(data);
   return (
     <Box>App here</Box>
   );
